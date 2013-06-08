a12445eb5bbae400d633d7df22a8357511919821
Alan Brockett <abrockett@gmail.com>
2013-06-07 19:03:05 -0600

    Now shows (incorrect) chart

--- App.js
+++ App.js
@@ -1,65 +1,79 @@
-Ext.define('CustomApp', {
+Ext.define('DefectTrendApp', {
     extend: 'Rally.app.App',
-	requires: [ 'ProdVsTestCalculator'],
+    requires: [ 'ProdVsTestCalculator'],
     componentCls: 'app',
 
-    launch: function() {
-        this.snapshotStore = Ext.create('Rally.data.lookback.SnapshotStore', {
-			// context: {
-			// 	workspace: '/workspace/' + this.getContext().getWorkspace().ObjectID,
-			// 	project: '/project/' + this.getContext().getProject().ObjectID,
-			// 	projectScopeUp: this.getContext().getProjectScopeUp(),
-			// 	projectScopeDown: this.getContext().getProjectScopeDown()
-			// },
-            listeners: {
-                load: function(store, data, success) {
-                    this._processData(store, data, success);
-                },
-                scope: this
-            },
-            fetch: ['Name','Environment','OpenedDate','Project'],
-            hydrate: ['Environment'],
-		    filters: [
-		    	{
-		    		property: '_ProjectHierarchy',
-		    		operator: '=',
-		    		value: this.getContext().getProject().ObjectID
-		    	},
-                {
-                    property: '_TypeHierarchy',
-                    operator: '=',
-                    value: 'Defect'
-                },
-                {
-                    property: 'Resolution',
-                    operator: '=',
-                    value: 'Duplicate'
-                },
-                // {
-                //     property: '__At',
-                //     value: '2013-05-28T00:00:00Z'
-                // }
-                {
-                	property: '_ValidFrom',
-                	operator: '=',
-                	value: {
-						$gte: "2010-02-01TZ",
-						$lte: "current"
-					}
-                }
-            ]
-        });
-        this.snapshotStore.load();
-    },
+    items: [
+        {
+            xtype: 'rallychart',
 
-    _processData: function(store, data, success) {
-    	console.log(data.length);
-    	// Ext.Array.each(data, function(item) {
-    	// 	console.log(item.data.Environment + '\t' + item.data.OpenedDate + '\t' + item.data.Project + '\t' + item.data.Name);
-    	// });
-debugger;
-    	this.calculator = Ext.create('ProdVsTestCalculator', {});
-    	this.chartData = this.calculator.prepareChartData(store);
-    	console.log(chartData);
-    }
+            storeConfig: {
+                fetch: ['Name','Environment','OpenedDate','Project'],
+                hydrate: ['Environment'],
+                filters: [
+                    // {
+                    //     property: '_ProjectHierarchy',
+                    //     operator: '=',
+                    //     value: this.getContext().getProject().ObjectID
+                    // },
+                    {
+                        property: '_TypeHierarchy',
+                        operator: '=',
+                        value: 'Defect'
+                    },
+                    {
+                        property: 'Resolution',
+                        operator: '=',
+                        value: 'Duplicate'
+                    },
+                    {
+                        property: '_ValidFrom',
+                        operator: '=',
+                        value: {
+                            $gte: "2010-02-01TZ",
+                            $lte: "current"
+                        }
+                    }
+                ]
+            },
+
+            calculatorType: 'ProdVsTestCalculator',
+            calculatorConfig: {
+            	granularity: 'month'
+            },
+
+            chartConfig: {
+                chart: {
+                    zoomType: 'xy'
+                },
+                title: {
+                    text: 'Defect Trends'
+                },
+                xAxis: {
+                    tickmarkPlacement: 'on',
+                    tickInterval: 20,
+                    title: {
+                        text: 'Days'
+                    }
+                },
+                yAxis: [
+                    {
+                        title: {
+                            text: 'Count'
+                        }
+                    }
+                ],
+                plotOptions: {
+                    series: {
+                        marker: {
+                            enabled: false
+                        }
+                    },
+                    area: {
+                        stacking: 'normal'
+                    }
+                }
+            }
+        }
+    ]
 });
